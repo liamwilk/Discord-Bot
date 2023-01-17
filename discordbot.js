17ef5d818edb77d86c02be52f809d654f34f52d7
Snipert01 <33577925+Snipert01@users.noreply.github.com>
2023-01-17 17:15:33 -0300

    commit

--- discordbot.js
+++ discordbot.js
@@ -1,5 +1,21 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+require('dotenv').config();
+
+const keepAlive = require('./server');
+const Monitor = require('ping-monitor');
+ 
+keepAlive();
+const monitor = new Monitor({
+    website: 'LINK',
+    title: 'Nombre',
+    interval: 30 // minutes
+});
+ 
+monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
+monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} se ha parado.`) );
+monitor.on('error', (error) => console.log(error));
 
 client.on("ready", () => {
   console.log("El bot esta listo!");
